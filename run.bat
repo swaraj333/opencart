99c41f9e8fec17b88ed1e2e981f8d7de3d06775f
swaraj333 <swaraj.panjala@gmail.com>
2021-09-17 21:18:41 +0530

    first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\admin\eclipse-workspace\Opencart123
+mvn clean test
---
+++ src/test/java/testCases/TC_004.java
@@ -0,0 +1,17 @@
+package testCases;
+
+public class TC_004 {
+// This test will be implemented in future..
+ public static void main(String args[])
+ {
+System.out.println("this is change done by X");
+}
+ 
+ 
+ //something
+ //something
+ //something
+ //something
+ //something
+ //something
+}
